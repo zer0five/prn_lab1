2c792f691cb824f77b1f83bdc209077cd36e80b3
Radkaim <hungct04@gmail.com>
2022-09-22 13:50:13 +0700

    add Imarks interface

---
+++ IMarks.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prn_lab1
+{
+    public interface IMarks
+    {
+       abstract void inputMarks();
+       abstract void printMarks();
+
+    }
+}

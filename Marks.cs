85db15ae4ff4f256c024fee1d42bb31173c6cc4f
Radkaim <hungct04@gmail.com>
2022-09-22 15:00:22 +0700

    fix While loop

--- Marks.cs
+++ Marks.cs
@@ -13,7 +13,7 @@ namespace prn_lab1
             get { return _marks; }
             set
             {
-                while (value >= 0 || value <= 100)
+                while (value < 0 || value > 100)
                 {
                     Console.WriteLine("Mark must be in range 0-100");
                     Console.Write("Enter mark again: ");
@@ -30,7 +30,7 @@ namespace prn_lab1
             get { return _passLevel; }
             set
             {
-                while (value >= 40 || value <= 100)
+                while (value < 40 || value > 100)
                 {
                     Console.WriteLine("Pass level must be in range 40-100");
                     Console.Write("Enter pass level again: ");

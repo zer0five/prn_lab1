b7b9330871f6681eccd9c8c1eafc7a8a76d8d55d
K4zoku <kazoku0356@gmail.com>
2022-09-22 13:03:46 +0700

    Inital commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prn_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

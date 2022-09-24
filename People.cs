0b105aa2e505cbde0a30ef65ae7585c122e8f258
quocviet <thaibinhquocviet2002@gmail.com>
2022-09-24 13:54:35 +0700

    Input address , phone

--- People.cs
+++ People.cs
@@ -36,6 +36,7 @@ namespace prn_lab1
             get { return _fullName; }
             set
             {
+                //regex for letter
                 Regex regex = new Regex(@"^[a-zA-Z\s]*$");
                 //validate fullName
 
@@ -54,18 +55,52 @@ namespace prn_lab1
         public DateTime Birthday
         {
             get { return _birthday; }
-            set { _birthday = value; }
+            set
+            {
+                _birthday = value;
+                while (true)
+                {
+                    if (value < new DateTime(1999, 1, 1) && value > DateTime.Today)
+                    {
+                        System.Console.Write("Invalid birthday, please try again: ");
+
+
+                    }
+                }
+
+            }
         }
+        private string _address;
+
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                //address not null
+                string s = value;
+
+                while (s.Length == 0)
+                {
+                    System.Console.Write("Address must not be empty: ");
+                    s = Console.ReadLine().Trim();
+                }
+                _address = s;
+            }
+        }
+
         private string _email;
         public string Email
         {
             get { return _email; }
             set
             {
+                //regex for email validate
                 Regex re = new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
                                     @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
                                      @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
                 string s = value;
+                //validate email address
                 while (!re.IsMatch(s) && s.Length > 0)
                 {
                     System.Console.Write("Invalid email,please enter again: ");
@@ -82,10 +117,20 @@ namespace prn_lab1
         public string Phone
         {
             get { return _phone; }
-            set { _phone = value; }
+            set
+            {
+
+                string s = value;
+                while (s.Length != 10 || !s.All(char.IsDigit) || !s.StartsWith("0"))
+                {
+                    System.Console.Write("Please enter valid phone number(e.g 0123456789): ");
+                    s = Console.ReadLine().Trim();
+                }
+                _phone = s;
+            }
         }
 
-        public People(string iDCard, string fullName, DateTime birthday, string email, string phone)
+        public People(string iDCard, string fullName, DateTime birthday, string email, string phone, string address)
         {
 
             IDCard = iDCard;
@@ -93,6 +138,7 @@ namespace prn_lab1
             Birthday = birthday;
             Email = email;
             Phone = phone;
+            Address = address;
         }
 
         public People()
@@ -108,8 +154,15 @@ namespace prn_lab1
             System.Console.Write("Enter full name: ");
             FullName = Console.ReadLine();
             //todo input birthday
+
             System.Console.Write("Enter email address: ");
             Email = Console.ReadLine();
+            //todo : adresss
+            System.Console.Write("Enter address: ");
+            Address = Console.ReadLine();
+            System.Console.Write("Enter phone number (e.g 0123456789): ");
+            Phone = Console.ReadLine();
+
 
         }
         // public abstract void printInfo();
--- Program.cs
+++ Program.cs
@@ -7,18 +7,10 @@ namespace prn_lab1
         static void Main(string[] args)
         {
 
-<<<<<<< HEAD
-
-
-
-
-
-=======
-            // People p = new People();
-            // p.inputPeople();
-            Student s = new Student();
-            s.inputStudent();
->>>>>>> dfcedf921e0a2e7076996a32fff79574e05d3844
+            People p = new People();
+            p.inputPeople();
+            // Student s = new Student();
+            // s.inputStudent();
         }
     }
 }
